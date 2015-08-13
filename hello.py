614bf20e27d61d2df4c08cc516a12a41f142b291
hugesoft <hugesoft@126.com>
2015-08-13 23:47:11 +0800

    modified:   hello.py 	new file:   templates/404.html 	new file:   templates/500.html 	new file:   templates/base.html 	modified:   templates/user.html 	renamed:    templates/user.html -> templates/user2.html

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask,make_response,redirect,abort
 from flask.ext.script import Manager
 from flask import render_template
-
+from flask.ext.bootstrap import Bootstrap
+    
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -29,6 +31,18 @@ def get_user(id):
 @app.route('/login/<name>')
 def user(name):
     return render_template('user.html', username = name)
+
+@app.route('/404')
+def show_404():
+    return render_template('404.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
 #    app.run(debug = True)
