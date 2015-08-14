abd89e2bd72979cafa3fcca69d91db40e8679c50
root <hugesoft@126.com>
2015-08-14 16:59:21 +0800

    修改：     hello.py 	新文件：   templates/.index.html.swp 	修改：     templates/404.html 	修改：     templates/500.html 	修改：     templates/base.html 	修改：     templates/index.html 	新文件：   templates/index2.html 	修改：     templates/user.html

--- hello.py
+++ hello.py
@@ -2,20 +2,39 @@ from flask import Flask,make_response,redirect,abort
 from flask.ext.script import Manager
 from flask import render_template
 from flask.ext.bootstrap import Bootstrap
-    
+from flask.ext.moment import Moment
+from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.debug = True
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        return render_template('index.html', form=form, name=name)
+#        return render_template('index.html')
+"""
 @app.route('/cookie')
 def cookie():
     response = make_response('<h1>This docment carries a cookie!</h1>')
     response.set_cookie('answer','mmx')
     return response
+"""
 
 @app.route('/url/<url>')
 def url(url):
@@ -31,10 +50,6 @@ def get_user(id):
 @app.route('/login/<name>')
 def user(name):
     return render_template('user.html', username = name)
-
-@app.route('/404')
-def show_404():
-    return render_template('404.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
